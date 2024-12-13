3ae4fdd219ef012ff44797b51f8dd7e1c50a4bf3
Sk Samim <sekhsamim187@gmail.com>
2024-12-14 02:38:55 +0530

    code commented out

--- conditional-expression.tf
+++ conditional-expression.tf
@@ -1,17 +1,17 @@
-variable "environment" {
-  default = "production"
-}
-
-# resource "aws_instance" "myec2" {
-#  ami = "ami-0614680123427b75e"
-#  instance_type = var.environment == "development" ? "t2.micro" : "m5.large"
+# variable "environment" {
+#   default = "production"
 # }
 
-variable "region" {
- default = "ap-south-1" 
-}
+# # resource "aws_instance" "myec2" {
+# #  ami = "ami-0614680123427b75e"
+# #  instance_type = var.environment == "development" ? "t2.micro" : "m5.large"
+# # }
 
-resource "aws_instance" "my_ec2" {
-  ami = "ami-0614680123427b75e"
-  instance_type = var.environment == "production" && var.region == "ap-south-1" ? "m5.large" : "t2.micro"
-}
+# variable "region" {
+#  default = "ap-south-1" 
+# }
+
+# resource "aws_instance" "my_ec2" {
+#   ami = "ami-0614680123427b75e"
+#   instance_type = var.environment == "production" && var.region == "ap-south-1" ? "m5.large" : "t2.micro"
+# }
