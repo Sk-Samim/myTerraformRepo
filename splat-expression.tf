c742c306633bc2a28216eebbd26fe598b280cc2b
Sk Samim <sk.samim1@wipro.com>
2025-01-05 19:32:39 +0530

    please ignore this commit

--- splat-expression.tf
+++ splat-expression.tf
@@ -13,3 +13,20 @@
 # output "security_group_ids" {
 #   value = aws_security_group.example[*].id
 # }
+
+# variable "mymap" {
+#   default = {
+#     dev = "ami-123"
+#     prod = "ami-456"
+#   }
+# }
+
+# resource "aws_instance" "web" {
+#   for_each = var.mymap
+#   ami = each.value
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = each.key
+#   }
+# }
