5e36a7be1d40a688e6a686d996e1698b2a03bb29
Sk Samim <sekhsamim187@gmail.com>
2025-01-20 03:26:37 +0530

    Working with S3 state lock with the help of DynamoDB table

--- s3Banckend.tf
+++ s3Banckend.tf
@@ -1,7 +1,13 @@
+//for the first time, in order to execute this code you need to run "terraform init -reconfigure" command
 terraform {
   backend "s3" {
     bucket = "sk-samim-terraform-backend"
     key = "network/terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "terraform-backend-s3-state-lock-table"
   }
+}
+
+resource "time_sleep" "wait_150_seconds" {
+  create_duration = "150s"
 }
